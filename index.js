183e92c7b8f39e2f2ce0935034eb3608c90f4446
ishaan-codes <ishaangupta.246@gmail.com>
2024-07-17 10:11:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import express from "express";
+import nodemon from "nodemon";
+const app = express();
+const port = process.env.PORT;
+
+app.get("/",(req, res) => {
+    res.send("Hello World");
+})
+app.get("/about", (req, res) => {
+    res.send("<h1>Hi this is Ishaan<h1>");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${port}.`);
+})
+
